bee0d7afd01e0f5ba4bf9950e43a8cf94161d55e
karolinakonior <karolina.koniorr@gmail.com>
2024-01-01 19:46:30 +0000

    Completed Palindrome task

---
+++ src/App.java
@@ -0,0 +1,8 @@
+class App {  
+    public static void main(String[] args) {
+        Palindrome p = new Palindrome();
+        System.out.println(p.isPalindrome(121));
+        System.out.println(p.isPalindrome(-121));
+        System.out.println(p.isPalindrome(10));
+       }
+} 
---
+++ src/MainFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+    public void init() {
+        this.setTitle("Hello World");
+        this.setSize(400, 400);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+}
